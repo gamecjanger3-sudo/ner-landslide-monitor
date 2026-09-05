fba48bf2e92ddfaffe95b767f68f18724a608999
Saurabh <gamecjanger3@gmail.com>
2026-09-05 13:33:14 +0530

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      external: [
-        'express',
-        'cors',
-        'dotenv',
-        'multer',
-        '@google-cloud/translate'
-      ]
-    }
-  }
+  },
 });
